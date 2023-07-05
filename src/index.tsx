2c32d46a23f93d4e32e036eb655848f817bebf3e
sunny-dubey <97658562+sunny-dubey@users.noreply.github.com>
2023-07-05 16:32:19 +0530

    caching for better performance

--- src/index.tsx
+++ src/index.tsx
@@ -1,51 +1,57 @@
-import ReactDOM from 'react-dom';
-import { useState, useEffect, useRef } from 'react';
 import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
 
-const App=()=>{
-  const [input,setInput] = useState('');
-  const [code,setCode] = useState('');
+const App = () => {
   const ref = useRef<any>();
-  
-  const startService = async ()=>{
-      ref.current =  await esbuild.startService({
-        worker: true,
-        wasmURL: '/esbuild.wasm'
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
 
-      })    
-  }
-  useEffect(()=>{
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
+    });
+  };
+  useEffect(() => {
     startService();
-  },[])
-   
-  const onClick = async ()=>{
-    if(!ref.current){
+  }, []);
+
+  const onClick = async () => {
+    if (!ref.current) {
       return;
     }
+
     const result = await ref.current.build({
-      entryPoints:['index.js'],
+      entryPoints: ['index.js'],
       bundle: true,
       write: false,
-      plugins: [unpkgPathPlugin()],
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
       define: {
         'process.env.NODE_ENV': '"production"',
-        global: 'window'
-      }
-    })
-    
+        global: 'window',
+      },
+    });
+
+    // console.log(result);
+
     setCode(result.outputFiles[0].text);
   };
 
   return (
     <div>
-      <textarea value={input} onChange={e=> setInput(e.target.value)}></textarea>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
-    </div> 
-  )
-}
+    </div>
+  );
+};
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
